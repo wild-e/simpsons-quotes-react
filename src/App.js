9a3811d6b0e3846bf53ec8ffa6f56e00ddf30c4b
wild-e <wild.emmanuelle@gmail.com>
2021-08-12 21:00:37 +0200

    to be continued

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import Navbar from './components/Navbar';
-import QuoteCard from './components/QuoteCard';
+import QuoteList from './components/QuoteList';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Navbar
-        simpsonQuotesUrl = "https://fr.wikipedia.org/wiki/Les_Simpson"
-      />
-      <QuoteCard 
-        image = "https://pm1.narvii.com/7070/aeaeadea89d89327a8e5f4f83d10272dc337425fr1-1200-1200v2_128.jpg"
-        quote = " 'Shoplifting is a victimless crime, like punching someone in the dark.' "
-        character = "Nelson Muntz"
-      />
-      <QuoteCard 
-        image = "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FRalphWiggum.png?1497567511523"
-        quote = " 'That's where I saw the leprechaun...He told me to burn things.' "
-        character = "Ralph Wiggum"
-      />
+      <QuoteList/>
     </div>
   );
 }
---
+++ src/components/QuoteList.js
@@ -0,0 +1,48 @@
+import QuoteCard from './QuoteCard';
+
+// An array of objects
+const quotes = [
+  {
+    quote:
+      "Facts are meaningless. You could use facts to prove anything that's even remotely true.",
+    character: "Homer Simpson",
+    image:
+      "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939"
+  },
+  {
+    quote: "Nothing you say can upset us. We're the MTV generation.",
+    character: "Bart Simpson",
+    image:
+      "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FBartSimpson.png?1497567511638"
+  },
+  {
+    quote: "That's where I saw the leprechaun...He told me to burn things.",
+    character: "Ralph Wiggum",
+    image:
+      "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FRalphWiggum.png?1497567511523"
+  },
+  {
+    quote:
+      "Hello, Simpson. I'm riding the bus today because Mother hid my car keys to punish me for talking to a woman on the phone.",
+    character: "Principal Skinner",
+    image:
+      "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FSeymourSkinner.png?1497567511460"
+  }
+];
+
+
+const QuoteList = () => (
+
+  <div>
+    {quotes.map(item => (
+        <QuoteCard 
+            image = {item.image}
+            quote = {item.quote}
+            character = {item.character}
+            key={item.quote}
+        />
+    ))}
+  </div>
+);
+
+export default QuoteList;
